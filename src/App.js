bc32f7e7f1bacce2e14d201eb4a0a31a3e7bbab3
Dawson M <dawson.mortenson@gmail.com>
2018-02-01 14:57:00 -0800

    restructure the project to export the components in bulk from a common dir

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
 import { Text, View } from 'react-native';
+import { Header } from './components/common';
 
 export default class App extends Component<{}> {
   render() {
     return (
       <View>
-        <Text>Firebase is cool!</Text>
+        <Header heading="Tippy top"/>
       </View>
     );
   }
