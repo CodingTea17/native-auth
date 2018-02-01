d8ac7a801c8299086771318e706a2e699a1fe24b
Dawson M <dawson.mortenson@gmail.com>
2018-02-01 15:10:48 -0800

    add firebase and dotenv packages to project and init firebase in the App component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,21 @@
 import React, { Component } from 'react';
 import { Text, View } from 'react-native';
+import firebase from 'firebase';
 import { Header } from './components/common';
+import { APIKEY, AUTHDOMAIN, DATABASEURL, PROJECTID, STORAGEBUCKET, MESSAGINGSENDERID } from 'react-native-dotenv'
 
-export default class App extends Component<{}> {
+class App extends Component {
+  componentWillMount() {
+    firebase.initializeApp({
+      apiKey: APIKEY,
+      authDomain: AUTHDOMAIN,
+      databaseURL: DATABASEURL,
+      projectId: PROJECTID,
+      storageBucket: STORAGEBUCKET,
+      messagingSenderId: MESSAGINGSENDERID
+    });
+  }
+
   render() {
     return (
       <View>
@@ -11,3 +24,5 @@ export default class App extends Component<{}> {
     );
   }
 }
+
+export default App;
