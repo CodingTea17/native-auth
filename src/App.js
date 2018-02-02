2ac1144347a3af5fd9381c5a026ad96a3b9192cf
Dawson M <dawson.mortenson@gmail.com>
2018-02-01 20:14:27 -0800

    add cardsection to button and implment logout

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React, { Component } from 'react';
 import { Text, View } from 'react-native';
 import firebase from 'firebase';
-import { Header } from './components/common';
+import { Header, Button, Spinner, CardSection } from './components/common';
 import LoginForm from './components/LoginForm';
 import { APIKEY, AUTHDOMAIN, DATABASEURL, PROJECTID, STORAGEBUCKET, MESSAGINGSENDERID } from 'react-native-dotenv'
 
 class App extends Component {
+  state = {
+    loggedIn: null
+  }
   componentWillMount() {
     firebase.initializeApp({
       apiKey: APIKEY,
@@ -16,14 +19,33 @@ class App extends Component {
       messagingSenderId: MESSAGINGSENDERID
     });
 
-    console.log(APIKEY)
+    firebase.auth().onAuthStateChanged((user) => {
+      user ? this.setState({ loggedIn: true }) : this.setState({ loggedIn: false })
+    });
+  }
+
+  renderContent() {
+    switch (this.state.loggedIn) {
+      case true:
+        return (
+          <CardSection>
+            <Button onPress={() => firebase.auth().signOut()}>
+              Logout
+            </Button>
+          </CardSection>
+        );
+      case false:
+        return <LoginForm />;
+      default:
+        return <Spinner size='large' />;
+    }
   }
 
   render() {
     return (
       <View>
         <Header heading="Tippy top"/>
-        <LoginForm />
+        {this.renderContent()}
       </View>
     );
   }
